3f514723e3ee6e2f20a99a83abd1b61ff5abf574
Andrew <telepnev.1997@mail.ru>
2023-09-22 00:44:52 +0700

    add scripts

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+document.querySelector('#main-action-button').onclick = function () {
+  document.querySelector('#products').scrollIntoView({ behavior: 'smooth' });
+};
+
+let links = document.querySelectorAll('.menu-item > a');
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function () {
+    document
+      .querySelector('#' + links[i].getAttribute('data-link'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+}
+
+let buttons = document.querySelectorAll('.product-button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+    document.querySelector('.order').scrollIntoView({ behavior: 'smooth' });
+  });
+}
+
+let burger = document.querySelector('#burger'),
+  name = document.querySelector('#name'),
+  phone = document.querySelector('#phone');
+
+document.querySelector('#order-action').addEventListener('click', function () {
+  let hasError = false;
+
+  [burger, name, phone].forEach((item) => {
+    if (!item.value) {
+      item.parentElement.style.background = 'red';
+      hasError = true;
+    } else {
+      item.parentElement.style.background = '';
+    }
+  });
+
+  if (!hasError) {
+    [burger, name, phone].forEach((item) => {
+      item.value = '';
+    });
+    alert('Спасибо за заказ! Мы скоро свяжемся с вами!');
+  }
+});
+
+let prices = document.querySelectorAll('.products-item-price');
+
+document
+  .querySelector('#change-currency')
+  .addEventListener('click', function (e) {
+    let currentCurrency = e.target.innerText;
+
+    let newCurrency = '$';
+    let coefficient = 1;
+
+    if (currentCurrency === '$') {
+      newCurrency = '₽';
+      coefficient = 80;
+    } else if (currentCurrency === '₽') {
+      newCurrency = 'BYN';
+      coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+      newCurrency = '€';
+      coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    }
+
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prices.length; i++) {
+      prices[i].innerText =
+        +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) +
+        ' ' +
+        newCurrency;
+    }
+  });
